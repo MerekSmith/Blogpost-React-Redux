67ab9800e1d8994eb12856b3dec10fb43a0cac3b
Merek Smith <mereksmith@hotmail.com>
2018-10-23 21:34:55 -0600

    completed through lecture 82

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 // import App from './components/app'; --This is no longer needed due to using React Router. We don't really need that central file now.
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
