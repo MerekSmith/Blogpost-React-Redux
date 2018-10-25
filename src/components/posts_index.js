c58f8d1013877d156a9116255593b85a0e5422c1
Merek Smith <mesmith@overstock.com>
2018-10-25 16:34:19 -0600

    updated through lecture 86

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -7,14 +7,32 @@ class PostsIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        // Lodash is what enables us to be able to map through an object. The normal map function will only work on an Array.
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                    {post.content}
+                </li>
+            );
+        });
+    }
 
     render() {
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 // import App from './components/app'; --This is no longer needed due to using React Router. We don't really need that central file now.
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,9 +18,11 @@ ReactDOM.render(
     {/* <App /> */}
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        {/* <Route path="/posts/new" component={PostNew} /> */}
-        {/* <Route path="/posts/:id" component={PostShow} /> */}
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+          {/* <Route path="/posts/:id" component={PostsShow} /> */}
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
