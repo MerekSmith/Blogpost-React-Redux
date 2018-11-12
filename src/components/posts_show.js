ec77e4b80b0c740f6ad73d5015f5ee41e5be4222
Merek Smith <mereksmith@hotmail.com>
2018-11-12 06:37:52 -0700

    updated through lecture 103

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,22 +3,31 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
-    componentDidMount() {
-        const { id } = this.props.match.params.id;
-        this.props.fetchPost(id);
-    }
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
+	}
 
-    render() {
-        return (
-            <div>
-                Posts Show!
-                </div>
-        );
-    }
+	render() {
+		const { post } = this.props;
+		if (!post) {
+			return <div>Loading...</div>;
+		}
+
+		return (
+			<div>
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
+			</div>
+		);
+	}
 }
 
-function mapStateToProps({ posts }, ownProps) {
-    return { post: posts[ownProps.match.params.id] }
+function mapStateToProps(state, ownProps) {
+	const { posts } = state; //This { posts } can be placed directly above in place of state but I used this method to console log the state to see what it is providing.
+	console.log('this is the entire state',state);
+	return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
