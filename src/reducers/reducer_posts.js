c723aed7e0f01bbcf28a668c349f0c7420482862
Merek Smith <mesmith@overstock.com>
2018-11-13 13:10:12 -0700

    completed course

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
 	switch (action.type) {
+		case DELETE_POST:
+			return _.omit(state, action.payload); //action.payload will be the id of the post which was deleted, this is pasted through in the action creator function as its payload.
+			// this is good practice as it will immediately remove this from our local state so it does not briefly show up on the index page when they are rerouted.
 		case FETCH_POST:
 			// const post = action.payload.data;
 			// const newState = {...state };
